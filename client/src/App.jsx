e1fc5513e5203e50804a0abbbaa2c722f91c74c7
Arjunbir Singh <arjunbirsingh1699@gmail.com>
2025-06-12 23:25:06 +0530

    Refactored entry files and global styles for better structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom'
 import { AnimatePresence } from 'framer-motion'
+import { Toaster } from 'sonner'
 import Home from './pages/Home'
 import UserDashboard from './pages/UserDashboard'
 import AdminDashboard from './pages/AdminDashboard'
@@ -8,36 +9,90 @@ import Register from './pages/Register'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import PageTransition from './components/PageTransition'
+import { ThemeProvider } from './components/ThemeProvider'
+import { useEffect } from 'react'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 
 function AnimatedRoutes() {
   const location = useLocation();
   
   return (
     <AnimatePresence mode="wait">
-      <PageTransition key={location.pathname}>
-        <Routes location={location}>
-          <Route path="/" element={<Home />} />
-          <Route path="/user/dashboard" element={<UserDashboard />} />
-          <Route path="/admin/dashboard" element={<AdminDashboard />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-        </Routes>
-      </PageTransition>
+      <Routes location={location}>
+        <Route
+          path="/"
+          element={
+            <PageTransition key="home">
+              <Home />
+            </PageTransition>
+          }
+        />
+        <Route
+          path="/user/dashboard"
+          element={
+            <PageTransition key="user-dashboard">
+              <UserDashboard />
+            </PageTransition>
+          }
+        />
+        <Route
+          path="/admin/dashboard"
+          element={
+            <PageTransition key="admin-dashboard">
+              <AdminDashboard />
+            </PageTransition>
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <PageTransition key="login">
+              <Login />
+            </PageTransition>
+          }
+        />
+        <Route
+          path="/register"
+          element={
+            <PageTransition key="register">
+              <Register />
+            </PageTransition>
+          }
+        />
+      </Routes>
     </AnimatePresence>
   );
 }
 
 function App() {
+  useEffect(() => {
+    AOS.init({
+      duration: 800,
+      once: false,
+      mirror: true
+    });
+  }, []);
+
   return (
-    <Router>
-      <div className="flex flex-col min-h-screen bg-gradient-to-br from-gray-50 to-gray-100">
-        <Navbar />
-        <main className="flex-grow container mx-auto px-4 pt-24 pb-8">
-          <AnimatedRoutes />
-        </main>
-        <Footer />
-      </div>
-    </Router>
+    <ThemeProvider>
+      <Router>
+        <div className="min-h-screen flex flex-col">
+          <Navbar />
+          <main className="flex-1 container mx-auto px-4 py-8">
+            <AnimatedRoutes />
+          </main>
+          <Footer />
+          <Toaster 
+            position="top-center"
+            expand={false}
+            richColors
+            closeButton
+            theme="system"
+          />
+        </div>
+      </Router>
+    </ThemeProvider>
   )
 }
 
