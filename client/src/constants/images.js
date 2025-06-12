18821cbdd569e3749dbc06b4326dd700b72fec03
Arjunbir Singh <arjunbirsingh1699@gmail.com>
2025-06-12 23:26:15 +0530

    Added constants directory for managing reusable values

---
+++ client/src/constants/images.js
@@ -0,0 +1,7 @@
+export const images = {
+  hero: 'https://images.unsplash.com/photo-1534438327276-14e5300c3a48?q=80&w=2070',
+  login: 'https://images.unsplash.com/photo-1590487988256-9ed24133863e?q=80&w=2028',
+  register: 'https://images.unsplash.com/photo-1576678927484-cc907957088c?q=80&w=2067',
+  dashboard: 'https://images.unsplash.com/photo-1540497077202-7c8a3999166f?q=80&w=2070',
+  pattern: 'https://images.unsplash.com/photo-1544033527-b192daee1f5b?q=80&w=2070',
+};
