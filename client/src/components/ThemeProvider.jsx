5466aaaa8a1739d967769e1706a2e0bb1a50e99d
Arjunbir Singh <arjunbirsingh1699@gmail.com>
2025-06-15 16:21:18 +0530

    Refactored ThemeProvider.jsx to improve theme state management

--- client/src/components/ThemeProvider.jsx
+++ client/src/components/ThemeProvider.jsx
@@ -36,7 +36,7 @@ export const ThemeProvider = ({ children }) => {
         {theme === 'light' ? (
           <Moon className="w-5 h-5 text-gray-800" />
         ) : (
-          <Sun className="w-5 h-5 text-yellow-400" />
+          <Sun className="w-5 h-5 text-[#fec62c]" />
         )}
       </motion.button>
     </ThemeContext.Provider>
